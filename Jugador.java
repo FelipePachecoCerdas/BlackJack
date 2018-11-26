85d6eeb5e64141e4cb2b161e13104922bd5ea564
FelipePachecoCerdas <43649196+FelipePachecoCerdas@users.noreply.github.com>
2018-11-26 14:03:04 -0600

    Clase Jugador

--- Jugador.java
+++ Jugador.java
@@ -6,19 +6,66 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author felip
  */
 public class Jugador {
-  
+
+  private String nombre;
   private ArrayList<Carta> cartas;
-  
-  public Jugador() {
+  private boolean enJuego;
+  private int cantidadPasadas;
+
+  public Jugador(String pNombre) {
     cartas = new ArrayList<>();
-    
+    nombre = pNombre;
+    enJuego = true;
   }
-  
-  
+
+  public void perder() {
+    enJuego = false;
+    cantidadPasadas = 1;
+  }
+
+  public boolean estaEnJuego() {
+    return enJuego;
+  }
+
+  public void pasar() {
+    cantidadPasadas++;
+  }
+
+  public boolean haPasado() {
+    return cantidadPasadas > 0;
+  }
+
+  public void resetearPasadas() {
+    cantidadPasadas = 0;
+  }
+
+  public ArrayList<Carta> getCartas() {
+    return cartas;
+  }
+
+  public Carta getUltimaCarta() {
+    return cartas.get(cartas.size() - 1);
+  }
+
+  public void agregarCarta(Carta carta) {
+    cartas.add(carta);
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public int sumarCartas() {
+    int suma = 0;
+    for (Carta carta : cartas) {
+      suma += carta.getValorReal();
+    }
+    return suma;
+  }
+
 }
