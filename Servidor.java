b44c3b446babcb4b063ffc4f61d969ed9d32776e
Kendall <43555438+tenoreo@users.noreply.github.com>
2018-11-26 18:39:12 -0600

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -8,9 +8,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *@author Kendall Tenorio Chevez
+ *@author Felipe Pacheco Cerdas
  */
 public class Servidor {
   
